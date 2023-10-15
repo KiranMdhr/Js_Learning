bf549f2b3a14192099fd5c90c4a5c8986953de92
KiranManandhar <100822043+KiranMdhr@users.noreply.github.com>
2023-10-15 10:27:17 +0000

    data conversion

---
+++ Js_basis/data_conversion.js
@@ -0,0 +1,20 @@
+// let score = "kiran"
+
+// console.log(typeof score)
+
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33aaa"=> NaN
+// true => 1; fales=> 0
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let no = 55
+let stringNo = String(no)
+console.log(typeof stringNo);
