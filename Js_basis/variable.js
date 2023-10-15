65c7637073c0b5485bc7d636ea5f2b6325a85934
KiranManandhar <100822043+KiranMdhr@users.noreply.github.com>
2023-10-15 10:26:18 +0000

    data conversion

---
+++ Js_basis/variable.js
@@ -0,0 +1,20 @@
+const accountId = 1234
+let accountEmail = "kiranmdhr9@gmail.com"
+var accountPassword = "123456"
+accountCity = "Kathmandu"
+let accountState
+
+
+accountEmail = "kiranmdhr1@gmail.com"
+accountPassword = "123456789"
+accountCity = "Pokhara"
+
+
+console.log(accountEmail)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+Because it has got limited scope
+ */
