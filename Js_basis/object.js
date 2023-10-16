fc573901c53ca2e3dbd1ca627996f8970bfba217
KiranManandhar <100822043+KiranMdhr@users.noreply.github.com>
2023-10-16 04:25:47 +0000

    json destructioring

---
+++ Js_basis/object.js
@@ -0,0 +1,18 @@
+const course = {
+        coursename: "Js",
+        price: "999",
+        courseInstructor: "Kiran"
+}
+
+//course.courseInstructor
+
+const{courseInstructor : ins} = course
+console.log(ins)
+
+const{price: p} = course
+console.log(p)
+
+
+// API(xml structure mah aauthiyo aaile json mah aauxa)const course = {
+
+
