bb6b445750ae7f23bdb989252c79b0efc8452db8
KiranManandhar <100822043+KiranMdhr@users.noreply.github.com>
2023-10-16 05:24:18 +0000

    added some more

--- Js_basis/scope.js
+++ Js_basis/scope.js
@@ -1,12 +1,48 @@
 //Scope {}
 // var c = 100
 
-let a = 300
-if(true){
-    let a = 10
-    const b = 20
-    //  c = 30
-    console.log("Inner",`${a}`)
-}
+// let a = 300
+// if(true){
+//     let a = 10
+//     const b = 20
+//     //  c = 30
+//     console.log("Inner",`${a}`)
+// }
 
-console.log(a)
+// console.log(a)
+
+function one(){
+    const username ="kiran"
+    function two(){
+        const website = "youtube"
+        // console.log(username)
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+
+if(true){
+    const username = "ram"
+    if(username === "ram"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+// ++++++++++++++++++ interesting  +++++++++++++++
+
+function addOne(no) {
+    return no + 1
+
+}
+console.log(addOne(6))
+
+const addTwo = function(no){
+    return no + 2
+}
+addTwo(5)
