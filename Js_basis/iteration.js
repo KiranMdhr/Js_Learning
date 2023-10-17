bd0bba9cf2063e317e8e7770342fcb56e92dcc83
KiranManandhar <100822043+KiranMdhr@users.noreply.github.com>
2023-10-17 04:18:34 +0000

    added forof

--- Js_basis/iteration.js
+++ Js_basis/iteration.js
@@ -9,19 +9,29 @@
 
 // Multiplication Table upto 10
 
-for(i = 1 ;i<=10;i++){
-    console.log(`Multiplication table of ${i}`);
-    for(j = 1 ;j<=10;j++){
-        console.log(`${i} * ${j} = ${i * j }`);
-    }
+// for(i = 1 ;i<=10;i++){
+//     console.log(`Multiplication table of ${i}`);
+//     for(j = 1 ;j<=10;j++){
+//         console.log(`${i} * ${j} = ${i * j }`);
+//     }
     
-}
+// }
 
-const myArray = [10,20,30,40]
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    console.log(element);
+// const myArray = [10,20,30,40]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
     
+// }
+
+//for of 
+
+const arr = [1,2,3,45,56,54]
+
+for (const i of arr) {
+    console.log(i)
 }
-
-
+const greet = "hello nepal"
+for (const i of greet) {
+    console.log(i);
+}
