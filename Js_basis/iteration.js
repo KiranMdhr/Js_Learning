e110efa09674ab994fbc80e61892e66f4b537251
KiranManandhar <100822043+KiranMdhr@users.noreply.github.com>
2023-10-17 04:15:23 +0000

    some iteration

---
+++ Js_basis/iteration.js
@@ -0,0 +1,27 @@
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+    
+//     console.log(element)
+// }
+
+
+//nested loop
+
+// Multiplication Table upto 10
+
+for(i = 1 ;i<=10;i++){
+    console.log(`Multiplication table of ${i}`);
+    for(j = 1 ;j<=10;j++){
+        console.log(`${i} * ${j} = ${i * j }`);
+    }
+    
+}
+
+const myArray = [10,20,30,40]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
+
+
